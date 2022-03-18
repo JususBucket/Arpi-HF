0858e23973ae1ec3300b147823376ec3d2c3a7b3
JususBucket <91191272+JususBucket@users.noreply.github.com>
2022-03-18 22:41:09 +0100

    Add files via upload

---
+++ ArpiHF50%/app.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
+
+    var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 5000);
